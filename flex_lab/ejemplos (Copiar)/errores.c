84960e7c08804f44f142e906e04afd0462cc957c
tomas <tomas.b.h@ciencias.unam.mx>
2026-03-12 22:19:27 -0600

    pruebas

---
+++ flex_lab/ejemplos (Copiar)/errores.c
@@ -0,0 +1,4 @@
+int @variable = 10;     
+int 123variable = 5;   
+char c = '\x';          
+string s = "hola 
---
+++ flex_lab/ejemplos/numeros.c
@@ -0,0 +1,5 @@
+int a = 42;
+float b = 3.14;
+float c = 2.5e-3;
+float d = 1.23E+4;
+int oct = 0755;
